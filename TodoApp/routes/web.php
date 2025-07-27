25b723e537055037428806cbfbbf5e08e6bab366
sina99 <sina.ansari.901@gmail.com>
2025-07-26 21:48:56 -0700

    add route for index and store method

--- TodoApp/routes/web.php
+++ TodoApp/routes/web.php
@@ -1,9 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
+    Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store');
 });
 
 Route::middleware([
